8cae53a2f6601dd00a513913bb4c05f331b8d7dc
Ernesto Ricart <Ernesto Ricart>
2014-05-27 21:35:59 -0400

    Add first spec

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,5 +1,6 @@
+require 'spec_helper'
 describe Team do
-   before { @team = Team.new 5,4,5 }
+   before { @team = Team.new "Equipo" }
 
   subject { @team}
   it { should respond_to(:team_name, :filter_by_position, :add_player, :to_s)}
